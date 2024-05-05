2775d694c5e983f27f2f44d0e70615a1c7d5b90a
vedantrgosavi <vedantrgosavi@gmail.com>
2024-05-05 09:05:24 -0400

    results showing

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 #implements a registration form, storing the registrants in a dict, with error messages
-from flask import Flask, redirect, render_template, request
+from flask import Flask, redirect, render_template, g, request
 import sqlite3
 
 app = Flask(__name__)
 
-db = sqlite3.connect('froshims.db')
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect('froshims.db')
+    return g.db
 
-REGISTRANTS = {}
+@app.teardown_appcontext
+def close_db(e=None):
+    db = g.pop('db', None)
 
+    if db is not None:
+        db.close()
+        
 SPORTS = ["BasketBall", "Soccer", "Cricket", "American Football"]
 
 
@@ -15,38 +23,40 @@ SPORTS = ["BasketBall", "Soccer", "Cricket", "American Football"]
 def index():
     return render_template("index.html", sports=SPORTS)
 
-@app.route('deregister', methods=['POST'])
+@app.route('/deregister', methods=['POST'])
 def deregister():
     
     #forget the registrant 
+    db = get_db().cursor()
     id = request.form.get("id")
     if id: 
-        db.execute("Delete FROM registrants WHERE id = ?", id)
+        db.execute("DELETE FROM registrants WHERE id = ?", id)
+    get_db().commit()
     return redirect("/registrants")
 
 @app.route("/register", methods=["POST"])
 def register():
     
     #Validate Submission 
+    db = get_db().cursor()
     name = request.form.get("name")
-    if not name:
-        return render_template("error.html", message = "Missing name")
-    
-    #Validate sport
     sport = request.form.get("sport")
-    if not sport:
-        return render_template("error.html", message="Missing sport")
-    if sport not in SPORTS:
-        return render_template("error.html", message="Sport is Invalid/ not offered")
+    if not name or sport not in SPORTS:
+        return render_template("failure.html")
     
-    #Remeber registrant
-    REGISTRANTS[name] = sport 
+    #Remember registrant
+    db.execute("INSERT INTO registrants (name, sport) VALUES (?, ?)", (name, sport))
+    get_db().commit()
     
     #Confirm registration
     return redirect("/registrants")
 
 @app.route("/registrants")
 def registrants():
-    return render_template("registrants.html", registrants=REGISTRANTS)
+    db = get_db().cursor()
+    db.execute("SELECT * FROM registrants")
+    registrants = [dict(id=row[0], name=row[1], sport=row[2]) for row in db.fetchall()]
+    return render_template("registrants.html", registrants=registrants)
+
 
     
