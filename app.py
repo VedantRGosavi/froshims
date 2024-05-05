0dd3d4cfcb923e3967a22b3c0d1ce54030877a89
vedantrgosavi <vedantrgosavi@gmail.com>
2024-05-04 20:03:41 -0400

    First commit to this from vscode

---
+++ app.py
@@ -0,0 +1,52 @@
+#implements a registration form, storing the registrants in a dict, with error messages
+from flask import Flask, redirect, render_template, request
+import sqlite3
+
+app = Flask(__name__)
+
+db = sqlite3.connect('froshims.db')
+
+REGISTRANTS = {}
+
+SPORTS = ["BasketBall", "Soccer", "Cricket", "American Football"]
+
+
+@app.route('/')
+def index():
+    return render_template("index.html", sports=SPORTS)
+
+@app.route('deregister', methods=['POST'])
+def deregister():
+    
+    #forget the registrant 
+    id = request.form.get("id")
+    if id: 
+        db.execute("Delete FROM registrants WHERE id = ?", id)
+    return redirect("/registrants")
+
+@app.route("/register", methods=["POST"])
+def register():
+    
+    #Validate Submission 
+    name = request.form.get("name")
+    if not name:
+        return render_template("error.html", message = "Missing name")
+    
+    #Validate sport
+    sport = request.form.get("sport")
+    if not sport:
+        return render_template("error.html", message="Missing sport")
+    if sport not in SPORTS:
+        return render_template("error.html", message="Sport is Invalid/ not offered")
+    
+    #Remeber registrant
+    REGISTRANTS[name] = sport 
+    
+    #Confirm registration
+    return redirect("/registrants")
+
+@app.route("/registrants")
+def registrants():
+    return render_template("registrants.html", registrants=REGISTRANTS)
+
+    
